67646e5ad3ed46565e4c48cdd63b7d740049f573
SantiagoSuarezL <santiagosl1029@gmail.com>
2024-10-14 23:56:23 -0500

    Modificacion en la zona horaria

--- EsAbDeApp/models.py
+++ EsAbDeApp/models.py
@@ -1,35 +1,35 @@
-from datetime import timedelta, timezone
+from datetime import timedelta
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone as django_timezone
 
 class User(AbstractUser):
   failed_attempts = models.IntegerField(default=0)
   last_failed_attempt = models.DateTimeField(null=True, blank=True)
 
   groups = models.ManyToManyField(
-    'auth.Group',
-      related_name='custom_user_groups',  
+      'auth.Group',
+      related_name='custom_user_groups',
       blank=True
-    )
+  )
   user_permissions = models.ManyToManyField(
-    'auth.Permission',
-      related_name='custom_user_permissions',  
+      'auth.Permission',
+      related_name='custom_user_permissions',
       blank=True
-    )
+  )
 
   def is_locked_out(self):
-    if self.failed_attempts >= 5:
-      lockout_time = self.last_failed_attempt + timedelta(minutes=5)
-      if timezone.now() < lockout_time:
-        return True
-    return False
-  
+      if self.failed_attempts >= 5:
+          lockout_duration = django_timezone.now() - self.last_failed_attempt
+          return lockout_duration.total_seconds() < 300
+      return False
+
   def reset_failed_attempts(self):
-    self.failed_attempts = 0
-    self.last_failed_attempt = None
-    self.save()
+      self.failed_attempts = 0
+      self.last_failed_attempt = None
+      self.save()
 
   def register_failed_attempt(self):
-    self.failed_attempts += 1
-    self.last_failed_attempt = timezone.now()
-    self.save()
+      self.failed_attempts += 1
+      self.last_failed_attempt = django_timezone.now()
+      self.save()
