b04e08d16475c82668d46e8b6e98709ccbcd2f2a
SantiagoSuarezL <santiagosl1029@gmail.com>
2024-10-14 23:10:34 -0500

    Se creo la clase de UserAdmin para poder ver los usuarios en el panel administrativo

--- EsAbDeApp/admin.py
+++ EsAbDeApp/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+  list_display = ('username', 'email', 'failed_attempts', 'last_failed_attempt', 'is_active', 'is_staff') 
+  search_fields = ('username', 'email')
+  list_filter = ('is_active', 'is_staff')
+
+  fieldsets = (
+    (None, {
+        'fields': ('username', 'email', 'failed_attempts', 'last_failed_attempt', 'is_active', 'is_staff')
+    }),
+    ('Contraseña', {
+        'fields': ('password',)
+    }),
+  )
+
+  add_fieldsets = (
+    (None, {
+        'classes': ('wide',),
+        'fields': ('username', 'email', 'password1', 'password2', 'is_active', 'is_staff')}
+    ),
+  )
